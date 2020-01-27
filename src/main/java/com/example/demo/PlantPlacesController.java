018e7e923b53902ccb6f3aa8725c752c9383e5ec
Oludamilare Olukotun <olukotuno@gmail.com>
2020-01-27 00:40:14 -0500

    Add Javadoc, Add default / endpint

--- src/main/java/com/example/demo/PlantPlacesController.java
+++ src/main/java/com/example/demo/PlantPlacesController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlantPlacesController {
-	
+//	
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
 	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+	}
+
 
 }
